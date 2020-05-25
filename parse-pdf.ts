6c78f35a21cc444521287f55e75d42cdfaffc51f
bweez <brandonwebdev@gmail.com>
2020-05-25 00:41:57 -0700

    Added a note about reading the file locally

--- parse-pdf.ts
+++ parse-pdf.ts
@@ -4,9 +4,12 @@
 //   deno run --allow-write --allow-net https://gist.githubusercontent.com/Hopding/2d25620f0fac5cf141123129cbf0a3d8/raw/pdf-lib-deno-modify-script.ts
 
 import { PDFDocument, rgb, degrees } from 'https://cdn.pika.dev/pdf-lib@^1.6.0';
+
 // import { readFileStr, readFileStrSync } from "https://deno.land/std/fs/mod.ts";
 
+// Reading the file this way works, but the encoding is off somehow, PDFDocument.load can't find the pdf header
 // const file = await readFileStr("./annual-report-pdfs/2016.pdf", { encoding: "utf-8" });
+
 const url = 'https://digitalarchives.wa.gov/do/6F5FB22E627538839E4DBAC41BDDF87C.pdf';
 const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
